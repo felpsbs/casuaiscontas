636fd06269ce4e971e217b722d066960343156b3
Felipe Brandao <felipe.brandao97@outlook.com>
2020-07-08 17:17:30 -0300

    Ajustes nas páginas de erro e adicionada migração para criação de conta

--- casuaiscontas/src/main/java/br/com/casuaiscontas/controller/handler/ControllerAdviceExceptionHandler.java
+++ casuaiscontas/src/main/java/br/com/casuaiscontas/controller/handler/ControllerAdviceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.casuaiscontas.controller.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,7 +13,7 @@ public class ControllerAdviceExceptionHandler {
 	@ExceptionHandler(UserNotFoundException.class)
 	public ModelAndView userNotFoundException(UserNotFoundException e) {
 		ModelAndView mv = new ModelAndView("error");
-		mv.addObject("status", 404);
+		mv.addObject("status", HttpStatus.BAD_REQUEST.value());
 		return mv;
 	}
 	
